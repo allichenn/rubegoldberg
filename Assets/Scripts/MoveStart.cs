ee38f05765d30a21ba12ab26dd1c97108f1a8b22
allichenn <allisonchen24@hotmail.com>
2021-03-01 17:19:40 +0800

    final

--- Assets/Scripts/MoveStart.cs
+++ Assets/Scripts/MoveStart.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallBehavior : MonoBehaviour
+public class MoveStart : MonoBehaviour
 {
 
     SpriteRenderer myRenderer;
@@ -29,23 +29,8 @@ public class BallBehavior : MonoBehaviour
         //START HERE, AND CAN ONLY PRESS SPACE ONCE//
         if(spacePressed == false && Input.GetKeyDown(KeyCode.Space)){ //press down key once, returns true once
             //myBody.AddForce((Vector3.right + Vector3.up) * power);
-            myBody.AddForce((new Vector3(1, 0, 0) /*+ new Vector3(0, 1, 0)*/) * power);
+            myBody.AddForce((new Vector3(-1, 0, 0) /*+ new Vector3(0, 1, 0)*/) * power);
             spacePressed = true;
         }
     }
-
-    void OnCollisionEnter2D(Collision2D other){ //other is whatever the heck it collided into
-        if(other.gameObject.name == "trigger1"){ //must be double ==
-            myRenderer.color = trigger1Color;
-            Debug.Log("hit trigger 1");
-        }
-    }
-
-    /*void OnTriggerEnter2D(Collider2D other){ //COLLIDER not COLLISION bc no physics
-        if(other.gameObject.name == "gate"){ //must be double ==
-            myRenderer.color = gateColor;
-            Debug.Log("Hit gate");
-        }
-
-    }*/
 }
--- Assets/Scripts/heartColor.cs
+++ Assets/Scripts/heartColor.cs
@@ -2,24 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallBehavior : MonoBehaviour
+public class heartColor : MonoBehaviour
 {
 
     SpriteRenderer myRenderer;
-    public Color trigger1Color;
-    //public Color gateColor;
+    //public Color trigger1Color;
+    public Color heartsColor;
 
     Rigidbody2D myBody;
 
     public float power;
-    bool spacePressed;
+    //bool spacePressed;
 
     // Start is called before the first frame update
     void Start()
     {
         myRenderer = gameObject.GetComponent<SpriteRenderer>();
         myBody = gameObject.GetComponent<Rigidbody2D>();
-        spacePressed = false;
+        //spacePressed = false;
     }
 
     // Update is called once per frame
@@ -27,25 +27,25 @@ public class BallBehavior : MonoBehaviour
     {
         
         //START HERE, AND CAN ONLY PRESS SPACE ONCE//
-        if(spacePressed == false && Input.GetKeyDown(KeyCode.Space)){ //press down key once, returns true once
+        /*if(spacePressed == false && Input.GetKeyDown(KeyCode.Space)){ //press down key once, returns true once
             //myBody.AddForce((Vector3.right + Vector3.up) * power);
-            myBody.AddForce((new Vector3(1, 0, 0) /*+ new Vector3(0, 1, 0)*/) * power);
+            myBody.AddForce((new Vector3(1, 0, 0) + new Vector3(0, 1, 0)) * power);
             spacePressed = true;
-        }
+        }*/
     }
 
-    void OnCollisionEnter2D(Collision2D other){ //other is whatever the heck it collided into
+    /*void OnCollisionEnter2D(Collision2D other){ //other is whatever the heck it collided into
         if(other.gameObject.name == "trigger1"){ //must be double ==
             myRenderer.color = trigger1Color;
             Debug.Log("hit trigger 1");
         }
-    }
+    }*/
 
-    /*void OnTriggerEnter2D(Collider2D other){ //COLLIDER not COLLISION bc no physics
-        if(other.gameObject.name == "gate"){ //must be double ==
-            myRenderer.color = gateColor;
+    void OnTriggerEnter2D(Collider2D other){ //COLLIDER not COLLISION bc no physics
+        if(other.gameObject.name == "ball"){ //must be double ==
+            myRenderer.color = heartsColor;
             Debug.Log("Hit gate");
         }
 
-    }*/
+    }
 }
